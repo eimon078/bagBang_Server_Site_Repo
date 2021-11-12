e17fab1065bea658879d723c59c45b8a68d653fd
taief078 <eimonhossain078@gmail.com>
2021-11-12 17:10:49 +0600

    Added Reviews Api

--- index.js
+++ index.js
@@ -13,11 +13,80 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nxj01.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-console.log(uri);
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("connected");
+        const database = client.db('bag_bang');
+        const usersCollection = database.collection('users');
+        const reviewsCollection = database.collection('reviews')
+
+
+
+        //reviews post api
+        app.post('/reviews', async (req, res) => {
+            const review = req.body;
+            const result = await reviewsCollection.insertOne(review)
+            console.log(review);
+            res.json(result);
+        })
+        //reviews get Api
+        app.get('/reviews', async (req, res) => {
+            const cursor = reviewsCollection.find({});
+            const reviews = await cursor.toArray();
+            res.json(reviews);
+        })
+        //get  user role api
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
+        })
+        //user Post Api
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user)
+            console.log(user);
+            res.json(result);
+        })
+
+        //user put Api
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+        })
+
+        //Update User Role  Api
+        app.put('/users/admin', async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const filter = { email: user.email };
+            const updateDoc = { $set: { role: "admin" } };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.json(result);
+        })
+    }
+    finally {
+        // await client.close()
+    }
+}
+
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send("Doctors Portal Running");
+    res.send("BagBang Portal Running");
 })
 
 app.listen(port, () => {
